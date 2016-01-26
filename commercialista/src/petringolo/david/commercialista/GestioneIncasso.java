07587486c5506432b8fb234d16e80ec72782465e
davidPetringolo <david.petringolo@gmail.com>
2016-01-26 12:08:50 +0100

    Update GestioneIncasso.java

--- commercialista/src/petringolo/david/commercialista/GestioneIncasso.java
+++ commercialista/src/petringolo/david/commercialista/GestioneIncasso.java
@@ -2,25 +2,25 @@ package petringolo.david.commercialista;
 
 public class GestioneIncasso {
 	
-	private Cliente[] clienti = new Cliente[100];
-	private int nClienti = 0;
+	private Cliente[] _clienti = new Cliente[100];
+	private int _nClienti = 0;
 
-	public Cliente[] getClienti() {
-		return clienti;
+	public Cliente[] get_clienti() {
+		return _clienti;
 	}
 
-	public void setClienti(Cliente[] clienti) {
-		this.clienti = clienti;
+	public void set_clienti(Cliente[] _clienti) {
+		this._clienti = _clienti;
 	}
 	
-	public void addCliente(Cliente cliente) {
-		clienti[nClienti++] = cliente;
+	public void addCliente(Cliente _cliente) {
+		_clienti[_nClienti++] = _cliente;
 	}
 	
 	public int incassoTotale() {
 		int somma = 0;
-		for(int i = 0; i < nClienti; ++i) 
-			somma += clienti[i].debito();
+		for(int i = 0; i < _nClienti; ++i) 
+			somma += _clienti[i].debito();
 		
 		return somma;
 	}
